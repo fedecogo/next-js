1ae76e3510b1f7d0f426b40ef76b1212d7364df1
fedecogo <141748020+fedecogo@users.noreply.github.com>
2024-07-17 06:28:47 +0200

    Client-side-fetch

--- src/app/client-data-fatch/page.js
+++ src/app/client-data-fatch/page.js
@@ -1,9 +1,43 @@
-"use Client"
+"use client"
+
+import { useEffect, useState } from "react"
 
 
-export default function ClientSideDataFetching(){
+export default function ClientSideDataFetch(){
+
+        const [loading, setLoading] = useState(false);
+        const [user , setUser ] = useState([]);
+
+        async function fetchListOfUser (){
+            try{
+                setLoading(true)
+                const apiResponse = await fetch(`https://dummyjson.com/users`);
+                const result = await apiResponse.json();
+
+                if (result?.users){
+                        setUser(result.users);
+                        setLoading(false)
+                }
+
+            } catch(errror){
+                console.log(errror);
+                setLoading(false)
+
+            }
+        }
+
+        useEffect(()=>{
+            fetchListOfUser()
+        },[])
+    if(loading) return <h3 className="font-bold">loading</h3>
 
     return <div>
-        <h1>Client side data fetching</h1>
+           <ul>
+                    {user.map(user => ( 
+                        <li key={user.id} className="mt-3 cursor-pointer text-bold">
+                         {user.firstName} {user.lastName} - {user.email} 
+                        </li>
+                    ))}
+                </ul>
     </div>
 }
--- src/app/server-data-fetch/page.js
+++ src/app/server-data-fetch/page.js
@@ -5,15 +5,14 @@ async function fetchListOfUser() {
     try {
         const apiResponse = await fetch('https://dummyjson.com/users', { cache: 'force-cache' });
         
-        // Check if response is OK (status 200-299)
+        
         if (!apiResponse.ok) {
             throw new Error(`HTTP error! status: ${apiResponse.status}`);
         }
 
-        // Log the response to check its content
+       
         console.log(apiResponse);
         
-        // Check if apiResponse.json is a function
         if (typeof apiResponse.json !== 'function') {
             throw new TypeError('apiResponse.json is not a function');
         }
@@ -35,7 +34,7 @@ export default async function ServerSideDataFetching() {
             <div>
                 <h1>Server side data fetching</h1>
                 <ul>
-                    {listOfUser.users.map(user => ( // Assuming API response has a 'users' field
+                    {listOfUser.users.map(user => ( 
                         <li key={user.id} className="mt-3 cursor-pointer text-bold">
                             <Link href={`/server-data-fetch/${user.id}`}> {user.firstName} {user.lastName} - {user.email} </Link>
                         </li>
