5e8ebfddcc8ae1375aecb9d92e058e5211cab249
fedecogo <141748020+fedecogo@users.noreply.github.com>
2024-07-25 04:33:04 +0200

    useSWR

--- src/app/client-data-fatch/page.js
+++ src/app/client-data-fatch/page.js
@@ -1,44 +1,60 @@
 "use client"
 
-import { useEffect, useState } from "react"
-
+import { useEffect, useState } from "react";
+import useSWR from "swr";
+// funzione fetcher per gestire le richieste di rete
+const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 export default function ClientSideDataFetch(){
 
-        const [loading, setLoading] = useState(false);
-        const [user , setUser ] = useState([]);
+       // uso di SWR per la fetch dei dati
+        const { data, error, isLoading } = useSWR("https://dummyjson.com/users", fetcher)
 
-        async function fetchListOfUser (){
-            try{
-                setLoading(true)
-                const apiResponse = await fetch(`https://dummyjson.com/users`);
-                const result = await apiResponse.json();
 
-                if (result?.users){
-                        setUser(result.users);
-                        setLoading(false)
-                }
+        // const [loading, setLoading] = useState(false);
+        // const [user , setUser ] = useState([]);
 
-            } catch(errror){
-                console.log(errror);
-                setLoading(false)
+        // async function fetchListOfUser (){
+        //     try{
+        //         setLoading(true)
+        //         const apiResponse = await fetch(`https://dummyjson.com/users`);
+        //         const result = await apiResponse.json();
 
-            }
-        }
+        //         if (result?.users){
+        //                 setUser(result.users);
+        //                 setLoading(false)
+        //         }
 
-        useEffect(()=>{
-            fetchListOfUser()
-        },[])
-        
-    if(loading) return <h3 className="font-bold">loading</h3>
+        //     } catch(errror){
+        //         console.log(errror);
+        //         setLoading(false)
 
-    return <div>
-           <ul>
-                    {user.map(user => ( 
+        //     }
+        // }
+
+        // useEffect(()=>{
+        //     fetchListOfUser()
+        // },[])
+
+
+    if(error){
+        return <h1>{error}</h1>
+
+    }
+    if(isLoading) return <h3 className="font-bold">loading</h3>
+
+    return(
+    
+        <div>
+            <ul>
+                {data && data.users && data.users.length > 0
+                    ? data.users.map((user) => (
                         <li key={user.id} className="mt-3 cursor-pointer text-bold">
-                         {user.firstName} {user.lastName} - {user.email} 
+                            {user.firstName} {user.lastName} - {user.email}
                         </li>
-                    ))}
-                </ul>
-    </div>
+                      ))
+                    : null}
+            </ul>
+        </div>
+    )
 }
